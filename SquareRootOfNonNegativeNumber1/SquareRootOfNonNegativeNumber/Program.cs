4660d29cefa47f76d7677cb029e1066e062d2369
badri0909 <singuveerabhadrarao@gmail.com>
2022-05-04 09:40:13 +0530

    add .

---
+++ SquareRootOfNonNegativeNumber1/SquareRootOfNonNegativeNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SquareRootOfNonNegativeNumber
+{
+    public class Program
+    {
+        //static double squareRoot(double c, double l)
+        //{
+        //    double t = c;
+        //    //double t = c / t;
+        //    double root;
+        //    int count = 0;
+        //    while (true)
+        //    {
+        //        count++;
+        //        Math.Abs(t - c / t) > double.Epsilon)
+        //            break;
+
+        //    }
+
+        //}
+
+        public static void Main(string[] args)
+        {
+            //double c = 9;
+            //Console.WriteLine(Math.Sqrt(c));
+            double c = 9;
+            double t = c;
+            double root;
+            double epsilon = 1e-15;
+            root = Math.Abs(t - c / t) > double.Epsilon;
+
+
+        }
+    }
+}
